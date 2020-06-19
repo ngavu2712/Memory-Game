e7ad87cefb2b28d1369e6c16ad1cffa3c2dcae9f
Anna Vu <ngavu2712@gmail.com>
2020-06-18 22:39:13 -0700

    create shuffle function in app.js with winning and losing condition

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -8,8 +8,43 @@ import "./App.css";
 class App extends Component {
 
     // Initial state before mount
-    state = {};
+    state = {
+        status: "",
+        goal: 12,
+        score: 0,
+        futurama,
+        clickedCard : []
+    };
 
+    // function to shuffle the card when user clicks the card
+    shuffleCard = id => {
+
+        let clickedCard = this.state.clickedCard;
+
+        // If users click a card twice, they lose
+        if(clickedCard.include(id)){
+            this.setState( {clickedCard : [], score: 0, status: "Game Over! Click to Play Again!"})
+            return ;
+        } else {
+            clickedCard.push(id)
+        
+
+            // If the user do not repeat any card, they win
+            if(clickedCard === 12){
+                this.setState( {score: 12, status : "You Won! You must be a fan of Futurama!", clickedCard : []})
+                return;
+            }
+
+            //
+            this.setState({futurama, clickedCard, score : clickedCard.length, status: " "})
+
+                for (let i= futurama.length -1; i > 0; i--) 
+                {
+                    let j = Math.floor(Math.random() * (i+1))
+                    [futurama[i], futurama[j]] = [futurama[j], futurama[i]]
+                }
+        }
+    }
     render() {
 
         return 
