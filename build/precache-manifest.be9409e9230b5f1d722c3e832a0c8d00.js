84b0bd0f2f16544c625a261563f7df98131a32e0
Anna Vu <ngavu2712@gmail.com>
2020-06-24 17:37:45 -0700

    restructure directory for deployment on github

---
+++ build/precache-manifest.be9409e9230b5f1d722c3e832a0c8d00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d01096b593ee7bd1589fb6310f8396",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e40e2c120854ddae22a8",
+    "url": "/static/css/main.a9d17ad7.chunk.css"
+  },
+  {
+    "revision": "2dc3eaf0bbedb18cf0ed",
+    "url": "/static/js/2.188b53f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.188b53f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40e2c120854ddae22a8",
+    "url": "/static/js/main.100ca543.chunk.js"
+  },
+  {
+    "revision": "76c0879cc130edce90a2",
+    "url": "/static/js/runtime-main.2cea7679.js"
+  },
+  {
+    "revision": "dfab768118270b1d005306231d77f3d8",
+    "url": "/static/media/space.dfab7681.jpg"
+  }
+]);
